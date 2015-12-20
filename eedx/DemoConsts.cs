f8661bafa77b45a13611d14cd017875c193d3b06
eetumartola <eetu.martola@gmail.com>
2015-12-21 00:03:10 +0200

    Initial commit

---
+++ eedx/DemoConsts.cs
@@ -0,0 +1,8 @@
+﻿
+public static class DemoConsts
+{
+    public const int kWidth = 1600;
+    public const int kHeight = 900;
+    public const string kDemoTitle = "eetu SharpDX Demo";
+}
+
---
+++ eedx/Program.cs
@@ -0,0 +1,29 @@
+﻿using SharpDX.Windows;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace eedx
+{
+    class Program
+    {
+        private static RenderForm _renderForm;
+
+        static void Main(string[] args)
+        {
+            InitialiseRenderForm();
+            RenderLoop.Run(_renderForm, RenderCallback);
+        }
+
+        private static void InitialiseRenderForm()
+        {
+            _renderForm = new RenderForm(DemoConsts.kDemoTitle);
+            _renderForm.ClientSize = new Size(DemoConsts.kWidth, DemoConsts.kHeight);
+            _renderForm.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private static void RenderCallback()
+        {
+
+        }
+    }
+}
